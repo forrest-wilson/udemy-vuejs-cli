4b1fc1d4d7dc0233568e8e19d816f756ec294171
Forrest Wilson <forrest.wilson@me.com>
2018-04-18 22:49:58 +1200

    Components

--- src/Home.vue
+++ src/Home.vue
@@ -1,23 +1,16 @@
 <template>
 	<div>
-		<p>Server Status: {{ status }}</p>
-		<hr>
-		<button @click="changeStatus">Change Status</button>
+		<app-server-status :key="server" v-for="server in 5">{{ server }}</app-server-status>
 	</div>
 </template>
 
 <script>
-export default {
-	data: function() {
-		return {
-			status: "Critical"
-		}
-	},
-	methods: {
-		changeStatus() {
-			this.status = "Normal";
+	import ServerStatus from "./ServerStatus.vue";
+
+	export default {
+		components: {
+			"app-server-status": ServerStatus
 		}
 	}
-}
 </script>
 
